ebe48e9db4b59f471faabc0a36f35f2a0af3c84c
Daniel Nicácio <70770484+daniel-nicacio@users.noreply.github.com>
2022-05-08 07:56:53 -0300

    feat: add project back-end

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "dc877521814acc",
+    pass: "684fd4e6ce8367",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Daniel Nicácio <danielfigueiredo789@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
